b1c17a080480e0e720da0320a66cced75baf66e3
ziadehab74 <zehab028@gmail.com>
2025-06-11 17:08:36 +0300

    sda

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -28,7 +28,7 @@ const myProjects = [
     title: "Egyption Silos",
     subtitle: "It is a project that aims to mechanize Egyptian wheat silos and govern local and imported wheat across the country.",
     associatedWith: ["ACME SAICO", "IBM"],
-    image: "/public/silos.jpg",
+    image: "/silos.jpg",
     link: "https://almalnews.com/1089530/%D8%A7%D9%84%D8%AA%D9%85%D9%88%D9%8A%D9%86-%D8%AA%D8%B7%D8%A8%D9%82-%D9%85%D8%B4%D8%B1%D9%88%D8%B9-%D9%85%D9%8A%D9%83%D9%86%D8%A9-%D8%B5%D9%88%D8%A7%D9%85%D8%B9-%D8%A7%D9%84%D9%82%D9%85/"
   },
 ];
